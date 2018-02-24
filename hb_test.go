646ff9e22c6a84a60f933b6655c16193bee85734
Charles Duffy <charles.duffy@gmail.com>
2018-02-24 11:51:35 +1100

    Initial testing framework

---
+++ hb_test.go
@@ -0,0 +1,18 @@
+package main
+
+//go:generate rm -f parser/y.go
+//go:generate goyacc -o parser/y.go -p expr parser/gram.y
+//go:generate build/mkstructures.sh
+
+import (
+		"github.com/hatbox/parser"
+		"testing"
+)
+
+
+func TestSQLSelect1(t *testing.T){
+		parser.P.Parse(string("select foo from bar;"))
+
+		dd := parser.P.Mkdot()
+		dd.Drawdot()
+}
